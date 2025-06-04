509f029e395e681281950c831eae18d98e311abd
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-06-04 22:52:17 +0500

    initial commit

--- learning/views/courses.py
+++ learning/views/courses.py
@@ -1,3 +1,11 @@
 from learning.models import Course, Lesson
 from learning.serializers import CourseSerializer, LessonSerializer
 from rest_framework import viewsets
+from learning.permissions import IsStaffOrReadOnly, IsOwnerOrReadOnly
+
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
