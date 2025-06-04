53c04ae1161dca6f159c0ea4f37c2c53a015137f
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-06-04 22:35:48 +0500

    create models

--- learning/models/courses.py
+++ learning/models/courses.py
@@ -1,7 +1,21 @@
 from django.db import models
-from .profile import TeacherProfile, StudentProfile
+from .profile import TeacherProfile
 
-class Courses(models.Model):
+
+class Course(models.Model):
     title = models.CharField(max_length=50, null=False, blank=False)
     description = models.TextField()
-    teacher = models.ForeignKey(TeacherProfile, )
+    teacher = models.ForeignKey(TeacherProfile, on_delete=models.CASCADE, related_name='courses')
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons')
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    video_url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.course.title} - {self.title}'
